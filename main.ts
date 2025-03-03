4e3636f8c6585331402a78243d62206744742d9a
Ryan A. Bolson <cammellryan@gmail.com>
2025-03-03 09:19:26 +0000

    Initial Commit, adding ReactView.

--- main.ts
+++ main.ts
@@ -2,51 +2,71 @@ import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Set
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface MyPluginSettings
+{
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: MyPluginSettings =
+{
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
+export default class LootTablePlugin extends Plugin
+{
 	settings: MyPluginSettings;
 
-	async onload() {
+	// Called when user starts using the plugin, configure capabilities here:
+	async onload()
+	{
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		// This creates an icon in the left ribbon:
+		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) =>
+		{
+			// Called when the user clicks the icon:
+			new Notice('I love you Cloe very much!');
 		});
-		// Perform additional things with the ribbon
+
+		// Perform additional things with the ribbon:
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
+
+		const ribbonIcon0 = this.addRibbonIcon('dice', 'Sample Plugin2', (evt: MouseEvent) =>
+		{
+			new Notice('I love you Cloe x2!');
+		})
+
+		ribbonIcon0.addClass('my-plugin-ribbon-class');
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText('Status Bar Text');
 
 		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
+		this.addCommand(
+			{
 			id: 'open-sample-modal-simple',
 			name: 'Open sample modal (simple)',
 			callback: () => {
 				new SampleModal(this.app).open();
 			}
 		});
+
 		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
+		this.addCommand(
+			{
 			id: 'sample-editor-command',
 			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
+			editorCallback: (editor: Editor, view: MarkdownView) =>
+			{
 				console.log(editor.getSelection());
 				editor.replaceSelection('Sample Editor Command');
 			}
 		});
+
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
+		this.addCommand(
+			{
 			id: 'open-sample-modal-complex',
 			name: 'Open sample modal (complex)',
 			checkCallback: (checking: boolean) => {
@@ -70,52 +90,66 @@ export default class MyPlugin extends Plugin {
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+		this.registerDomEvent(document, 'click', (evt: MouseEvent) =>
+		{
 			console.log('click', evt);
 		});
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+
+		console.log('loading plugin');
 	}
 
-	onunload() {
-
+	// Called when plugin is disabled, release resources here:
+	onunload()
+	{
+		console.log('unloading plugin');
 	}
 
-	async loadSettings() {
+	async loadSettings()
+	{
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
 	}
 
-	async saveSettings() {
+	async saveSettings()
+	{
 		await this.saveData(this.settings);
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class SampleModal extends Modal
+{
+	constructor(app: App)
+	{
 		super(app);
 	}
 
-	onOpen() {
+	onOpen()
+	{
 		const {contentEl} = this;
 		contentEl.setText('Woah!');
 	}
 
-	onClose() {
+	onClose()
+	{
 		const {contentEl} = this;
 		contentEl.empty();
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class SampleSettingTab extends PluginSettingTab
+{
+	plugin: LootTablePlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: LootTablePlugin)
+	{
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
-	display(): void {
+	display(): void
+	{
 		const {containerEl} = this;
 
 		containerEl.empty();
@@ -126,7 +160,8 @@ class SampleSettingTab extends PluginSettingTab {
 			.addText(text => text
 				.setPlaceholder('Enter your secret')
 				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
+				.onChange(async (value) =>
+				{
 					this.plugin.settings.mySetting = value;
 					await this.plugin.saveSettings();
 				}));
